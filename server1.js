c51c5889eb4fb30ff826360932c49805b00ea5c3
Kimberly Wilks <wilks.kimberly@gmail.com>
2020-06-30 19:51:21 -0500

    Updated port

--- server1.js
+++ server1.js
@@ -3,7 +3,7 @@
 var express = require("express");
 
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Data
 // ===========================================================
@@ -11,14 +11,14 @@ var yoda = {
   name: "Yoda",
   role: "Jedi Master",
   age: 900,
-  forcePoints: 2000
+  forcePoints: 2000,
 };
 
 var darthmaul = {
   name: "Darth Maul",
   role: "Sith Lord",
   age: 200,
-  forcePoints: 1200
+  forcePoints: 1200,
 };
 
 // Create one more data entry for the character Obi Wan Kenobi.
@@ -31,15 +31,15 @@ var darthmaul = {
 
 // Routes
 // ===========================================================
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
   res.send("Welcome to the Star Wars Page!");
 });
 
-app.get("/yoda", function(req, res) {
+app.get("/yoda", function (req, res) {
   res.json(yoda);
 });
 
-app.get("/darthmaul", function(req, res) {
+app.get("/darthmaul", function (req, res) {
   res.json(darthmaul);
 });
 
@@ -53,6 +53,6 @@ app.get("/darthmaul", function(req, res) {
 
 // Listener
 // ===========================================================
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log("App listening on PORT " + PORT);
 });
